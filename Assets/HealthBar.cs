9fbf3c1ddbce080f65903c9385b28a96c142dced
Hols <samettcaner@gmail.com>
2022-07-16 15:13:23 +0300

    Update HealthBar

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -6,25 +6,37 @@ using Sirenix.OdinInspector;
 
 public class HealthBar : MonoBehaviour
 {
-   public float CurrentHealth;
-   public Image healthBar;
-   public float MaxHealth = 100f;
-   void Start() 
-   {
-    CurrentHealth = MaxHealth;
-   }
-   void Update() 
-   {
-    SetBar();
-   }
-   public void SetBar()
-   {
-    healthBar.fillAmount = (float)CurrentHealth / (float)MaxHealth;
-   }
+    public float CurrentHealth;
+    public Image healthBar;
+    public float MaxHealth = 100f;
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+    void Update()
+    {
+        SetBar();
+    }
+    public void SetBar()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (float)CurrentHealth / (float)MaxHealth, Time.deltaTime * 3);
+    }
 
     [Button("Decrease!")]
-   public void DecreaseHealth(int damage)
-   {
-    CurrentHealth -= damage;
-   }
+    public void DecreaseHealth(int damage)
+    {
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0)
+        {
+         Debug.Log("Death!");
+        }
+    }
+    public void HealHealth(int heal)
+    {
+        CurrentHealth += heal;
+        if (CurrentHealth >= MaxHealth)
+        {
+         CurrentHealth = MaxHealth;
+        }
+    }
 }
