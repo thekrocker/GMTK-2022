a660bf507ebcaae7d9a44a7a3da204160b58b2b3
safa <sdedeakay@yahoo.com>
2022-07-16 16:20:02 +0300

    refactor: Refactor health script

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -4,39 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using Sirenix.OdinInspector;
 
-public class HealthBar : MonoBehaviour
+public class Health : MonoBehaviour
 {
     public float CurrentHealth;
-    public Image healthBar;
     public float MaxHealth = 100f;
+    public float Ratio => CurrentHealth / MaxHealth;
+
     void Start()
     {
         CurrentHealth = MaxHealth;
     }
-    void Update()
-    {
-        SetBar();
-    }
-    public void SetBar()
-    {
-        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (float)CurrentHealth / (float)MaxHealth, Time.deltaTime * 3);
-    }
 
-    [Button("Decrease!")]
     public void DecreaseHealth(int damage)
     {
         CurrentHealth -= damage;
         if (CurrentHealth <= 0)
         {
-         Debug.Log("Death!");
+            Debug.Log("Death!");
         }
     }
+
+
     public void HealHealth(int heal)
     {
         CurrentHealth += heal;
         if (CurrentHealth >= MaxHealth)
         {
-         CurrentHealth = MaxHealth;
+            CurrentHealth = MaxHealth;
         }
     }
 }
---
+++ Assets/Healthbar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Health))]
+    public class Healthbar : MonoBehaviour
+    {
+        public float changeSpeed = 3f;
+        public Image healthBarImage;
+        
+        private Health _health;
+
+        private void Awake()
+        {
+            _health = GetComponent<Health>();
+        }
+
+        private void Update()
+        {
+            SetHealthBar();
+        }
+
+        public void SetHealthBar()
+        {
+            healthBarImage.fillAmount = Mathf.Lerp(healthBarImage.fillAmount, _health.Ratio, Time.deltaTime * changeSpeed);
+        }
+    }
+}
